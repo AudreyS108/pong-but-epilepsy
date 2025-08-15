36d7a701037e4d3dd1d3be92e2490c368d19d848
Audrey Shi <audrey.shi108@gmail.com>
2025-08-15 17:50:11 +1200

    Worked out the basic layout of my Pygame game, added paddles, and also added a layout to my README file.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# pong-but-epilepsy
+# Pong But Epilepsy
+
+This project is called Pong But Epilepsy.   
+It is my (third/fourth) submission for Hack Club's Athena Award.   
+As the name suggests, this project is designed to be a chaotic pong game with a variety of wacky features. 
+Play the game to find out how it works, but be warned that there will be some epilepsy-inducing elements!  
+__________________________________________________________
+What it is
+-
+This Python project is a fun game featuring many different technical elements and game modes. Here's a breakdown of the game:
+__________________________________________________________
+Why did I make this project?
+-
+__________________________________________________________
+This project was made by using ____ (python language, python library/moduels pygame, sns)
+__________________________________________________________
+While making this project, I was faced with different challenges.   
+* Challenge 1: (insert here)
+
+I think through the creation of this project ___
+__________________________________________________________
+Project Stats:
+-
+* Time spent: __hrs __ mins
+* Code editor used: VS Code
+* Coding language(s): Python
+__________________________________________________________
+Contact Information
+-
+Please email **audrey.shi108@gmail.com** for any inquiries, or message **AudreyS108** on Slack. :D
---
+++ pong-but-epilepsy.py
@@ -0,0 +1,53 @@
+#PLAN:
+#1. Have main pong game which is functional
+#2. Add epilepsy effect by flashing the screen with random rgb colors
+#3. Add a "First to ___ points wins" system + game over screen declaring the winner.
+#4. Create a main menu to start game
+#5. Add Epilepsy and non epilepsy modes (incl epilepsy warning)
+#6. Possibly add a "How to play" screen
+#7. Possibly add a pong bot with AI - single player
+#6. Optional: Add sound effects and music
+#Thats it for now
+
+
+import pygame
+import sys
+import random
+
+pygame.init()
+# Setting up screen dimensions
+width, height = 1280, 720
+
+screen = pygame.display.set_mode((width, height))
+screen.fill("black")
+pygame.display.set_caption("Pong but epilepsy")
+clock = pygame.time.Clock()
+
+# da paddles
+playerone = pygame.Rect(width - 110, (height/2) - 50, 10, 100)
+playertwo = pygame.Rect(110, (height/2) - 50, 10, 100)
+
+
+#Pygame loop
+while True:
+    # Quit Pygame window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #--- end pygame window quit
+
+    #------------SCREEN FILLS:-----------
+    # epilepsy mode:
+    #screen.fill(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  
+    # normal mode
+    screen.fill("black")
+
+    # draw the player paddles
+    pygame.draw.rect(screen, "white", playerone)
+    pygame.draw.rect(screen, "white", playertwo)
+
+    pygame.display.update()
+    # clock.tick : Frames per second
+    clock.tick(100) 
+    
