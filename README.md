af1d80a7eab4df38cde55322320de32fddb05f20
Audrey Shi <audrey.shi108@gmail.com>
2025-08-18 18:06:12 +1200

    Added to the README and added a scoring system. Working to create a menu system.

--- README.md
+++ README.md
@@ -17,10 +17,11 @@ This project was made by using the language Python and Python libraries/modules 
 * Python: The coding language used to make this project.   
 * PyGame: The primary Python Library used for this project. This library helped in creating the main structure of the pong game. It allowed for me to display various sprites on the game's screen, as well as creating physics for the objects created.  
 * Sys (System): Python module used for interacting with Python runtime environment. Also helped in debugging the code.
-* Random: The Python module used for areas involving randomising. In my project, I used it to generate random RGB colors to contribute the "epilepsy" effect in my game.
+* Random: The Python module used for areas involving randomising. In my project, I used it to generate random RGB colors to contribute the "epilepsy" effect in my game. Also, I used the "random.choice" function to set random speeds for the ball with each rebound, just to make it extra chaotic and unpredictable!
 __________________________________________________________
 While making this project, I was faced with different challenges.   
-* Challenge 1: (insert here)
+* Challenge 1: Creating the physics of the game elements: paddles and balls. This was probably one of the most difficult parts of the project. Pygame doesn't come with its own physics, unlike other game development platforms such as Unity. I used some if/else loops to let the ball change direction when certain criteria (e.g. if the top of the ball touches the top of the screen) was met. 
+* Challenge 2: 
 
 I think through the creation of this project ___
 __________________________________________________________
--- pong-but-epilepsy.py
+++ pong-but-epilepsy.py
@@ -34,7 +34,7 @@ p1score = 0
 p2score = 0
 
 ball = pygame.Rect((width/2) - 10, (height/2) - 10, 20, 20)
-xspeed, yspeed = random.choice([-1, 1]), random.choice([-1, 1])
+xspeed, yspeed = random.choice([-1, 1]), random.choice([-2, -1, 1, 2])
 
 #Pygame loop
 while True:
@@ -67,19 +67,23 @@ while True:
     
     # --------Ball physics------------
     if ball.y >= height:
-        yspeed = -1
+        yspeed =  random.choice([-2, -1, 1, 2])
     if ball.y <= 0:
-        yspeed = 1
+        yspeed =  random.choice([-2, -1, 1, 2])
 
     if ball.x <= 0: #Left side
         ball.center = width/2, height/2
         xspeed = random.choice([-1, 1])
-        yspeed = random.choice([-1, 1])
+        yspeed =  random.choice([-2, -1, 1, 2])
+        p2score += 1 #update the score
+        print("Player 1 score:", p1score, ", Player 2 score:", p2score)
 
     if ball.x >= width: #right side
         ball.center = width/2, height/2
         xspeed = random.choice([-1, 1])
-        yspeed = random.choice([-1, 1])
+        yspeed =  random.choice([-2, -1, 1, 2])
+        p1score += 1 #update the score
+        print("Player 1 score:", p1score, ", Player 2 score:", p2score)
     
     if ball.colliderect(playerone) and ball.x < playerone.right:
         xspeed = 1
@@ -88,6 +92,15 @@ while True:
 
     ball.x += xspeed * 1.4
     ball.y += yspeed * 1.4
+
+    # ----------SCORES DISPLAY-----------
+    scoreboardtext = font.render("Scoreboard", True, "white")
+    p1text = font.render(str("Player 1: " + str(p1score)), True, "white")
+    p2text = font.render(str("Player 2: " + str(p1score)), True, "white")
+    
+    scoreboardrect = scoreboardtext.get_rect(center=(width/2, 50))
+    p1_rect = p1text.get_rect(center=(width/2 - 200, 80))
+    p2_rect = p1text.get_rect(center=(width/2 + 200, 80))
 
     #------------SCREEN FILLS:-----------
     # Screen must be filled to prevent any weird trails
@@ -101,8 +114,12 @@ while True:
     pygame.draw.rect(screen, "white", playertwo)
     pygame.draw.circle(screen, "white", ball.center, 10)
 
+    screen.blit(p1text, p1_rect)
+    screen.blit(p2text, p2_rect)
+    screen.blit(scoreboardtext, scoreboardrect)
+
     pygame.display.update()
     # clock.tick : Frames per second
-    clock.tick(300)
+    clock.tick(250)
 
     
