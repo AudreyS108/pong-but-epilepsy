b7ca52552e0da1a358fd9286ee01948d81127387
Audrey Shi <audrey.shi108@gmail.com>
2025-08-20 15:12:38 +1200

    I made some changes to the "Challenges" section of my README. Additionally, I fixed some bugs in the scoring system+  ball phsyics. FInally, I started creating a menu for the game.

--- README.md
+++ README.md
@@ -13,17 +13,19 @@ Personal Project Evaluation and Reflection
 -
 I decided to make this project for a fun technology week competition at my school. The theme of the competition was "chaos". Since my projects weren't usually silly, I was sort of stumped on what to do. Eventually, a friend of mine (also participating in the game competition) suggested to give the viewer epilepsy by switching the background colors constantly and by using a wide range of sounds. Genius. Because of this, I decided to create a simple pong game, but with an epilepsy factor. Simple, yet absolutely wacky. Also, I recently learned how to create games using Pygame, so I used it to make my project. Due to the limited time participants had to create their games for the competition, I only managed to add the main epilepsy factor and working pong game elements. At some point after the competition, I found the unfinished game sitting in my computer files. So, I chose to complete it and present the complete version of Pong But Epilepsy, which included a functional game menu and toggles. 
 __________________________________________________________
-This project was made by using the language Python and Python libraries/modules PyGame, Sys and Random.
+This project was made by using the coding language Python and many different Python Libraries.
+ 
 * Python: The coding language used to make this project.   
 * PyGame: The primary Python Library used for this project. This library helped in creating the main structure of the pong game. It allowed for me to display various sprites on the game's screen, as well as creating physics for the objects created.  
 * Sys (System): Python module used for interacting with Python runtime environment. Also helped in debugging the code.
 * Random: The Python module used for areas involving randomising. In my project, I used it to generate random RGB colors to contribute the "epilepsy" effect in my game. Also, I used the "random.choice" function to set random speeds for the ball with each rebound, just to make it extra chaotic and unpredictable!
+* Time: Python library which, as the name states, is a library to do with time. Specifically, I used the "Sleep" function, which helped for count-downs to begin the game. 
 __________________________________________________________
 While making this project, I was faced with different challenges.   
 * Challenge 1: Creating the physics of the game elements: paddles and balls. This was probably one of the most difficult parts of the project. Pygame doesn't come with its own physics, unlike other game development platforms such as Unity. I used some if/else loops to let the ball change direction when certain criteria (e.g. if the top of the ball touches the top of the screen) was met. 
-* Challenge 2: 
+* Challenge 2: Small bug I had to fix. Occasionally, the ball wouldn't rebound properly off the top and bottom, and would disappear. I found the reason for this bug was because of an inaccuracy in some of my random.choice functions. Originally, when the ball touched the top of the screen, the ball had the possibility to move in a positive or negative gradient, which would cause the ball to ocassionally disappear off the screen. To fix it, I removed the positive gradients for when the ball touched the top of the screen, and vice-versa for when the ball touched the bottom of the screen.
 
-I think through the creation of this project ___
+I think through the creation of this project _____
 __________________________________________________________
 Project Stats
 -
--- pong-but-epilepsy.py
+++ pong-but-epilepsy.py
@@ -13,6 +13,7 @@
 import pygame
 import sys
 import random
+from time import sleep
 
 pygame.init()
 # Setting up screen dimensions
@@ -26,15 +27,15 @@ clock = pygame.time.Clock()
 font = pygame.font.SysFont("Arial", 30)
 
 # da paddles
-playerone = pygame.Rect(110, (height/2) - 50, 10, 100)
-playertwo = pygame.Rect(width - 110, (height/2) - 50, 10, 100)
+PlayerOne = pygame.Rect(110, (height/2) - 50, 10, 100)
+PlayerTwo = pygame.Rect(width - 110, (height/2) - 50, 10, 100)
 
 #Player scores
 p1score = 0
 p2score = 0
 
 ball = pygame.Rect((width/2) - 10, (height/2) - 10, 20, 20)
-xspeed, yspeed = random.choice([-1, 1]), random.choice([-2, -1, 1, 2])
+xspeed, yspeed = random.choice([-1, 1]), random.choice([-1, 1])
 
 #Pygame loop
 while True:
@@ -46,61 +47,52 @@ while True:
     #--- end pygame window quit
 
     # --------Paddle movement!----------
-    
+
     keyspressed = pygame.key.get_pressed()
 
     # Player One controls
     if keyspressed[pygame.K_w]:
-        if playerone.bottom < height:
-            playerone.top += 2 # Flipped controls contrary to real world logistics: designed to be extra confusing
+        if PlayerOne.bottom < height:
+            PlayerOne.top += 2 # Flipped controls contrary to real world logistics: designed to be extra confusing
     elif keyspressed[pygame.K_s]:
-        if playerone.top > 0:
-            playerone.bottom -= 2
-        
+        if PlayerOne.top > 0:
+            PlayerOne.bottom -= 2
+
     # Player Two controls
     if keyspressed[pygame.K_UP]:
-        if playertwo.bottom < height:
-            playertwo.top += 2 
+        if PlayerTwo.bottom < height:
+            PlayerTwo.top += 2 
     elif keyspressed[pygame.K_DOWN]:
-        if playertwo.top > 0:
-            playertwo.bottom -= 2
+        if PlayerTwo.top > 0:
+            PlayerTwo.bottom -= 2
     
     # --------Ball physics------------
     if ball.y >= height:
-        yspeed =  random.choice([-2, -1, 1, 2])
+        yspeed = random.choice([-2, -1])
     if ball.y <= 0:
-        yspeed =  random.choice([-2, -1, 1, 2])
+        yspeed = random.choice([1, 2])
 
     if ball.x <= 0: #Left side
         ball.center = width/2, height/2
         xspeed = random.choice([-1, 1])
         yspeed =  random.choice([-2, -1, 1, 2])
-        p2score += 1 #update the score
+        p1score += 1 #update the score
         print("Player 1 score:", p1score, ", Player 2 score:", p2score)
 
     if ball.x >= width: #right side
         ball.center = width/2, height/2
         xspeed = random.choice([-1, 1])
         yspeed =  random.choice([-2, -1, 1, 2])
-        p1score += 1 #update the score
+        p2score += 1 #update the score
         print("Player 1 score:", p1score, ", Player 2 score:", p2score)
     
-    if ball.colliderect(playerone) and ball.x < playerone.right:
+    if ball.colliderect(PlayerOne) and ball.x < PlayerOne.right:
         xspeed = 1
-    if ball.colliderect(playertwo) and ball.x < playertwo.left:
+    if ball.colliderect(PlayerTwo) and ball.x < PlayerTwo.left:
         xspeed = -1
 
     ball.x += xspeed * 1.4
     ball.y += yspeed * 1.4
-
-    # ----------SCORES DISPLAY-----------
-    scoreboardtext = font.render("Scoreboard", True, "white")
-    p1text = font.render(str("Player 1: " + str(p1score)), True, "white")
-    p2text = font.render(str("Player 2: " + str(p1score)), True, "white")
-    
-    scoreboardrect = scoreboardtext.get_rect(center=(width/2, 50))
-    p1_rect = p1text.get_rect(center=(width/2 - 200, 80))
-    p2_rect = p1text.get_rect(center=(width/2 + 200, 80))
 
     #------------SCREEN FILLS:-----------
     # Screen must be filled to prevent any weird trails
@@ -109,9 +101,19 @@ while True:
     # normal mode
     screen.fill("black")
 
+    # ----------SCORES DISPLAY-----------
+    scoreboardtext = font.render("Scoreboard", True, "white")
+    p1text = font.render(str("Player 1: " + str(p1score)), True, "white")
+    p2text = font.render(str("Player 2: " + str(p1score)), True, "white")
+    
+    scoreboardrect = scoreboardtext.get_rect(center=(width/2, 50))
+    p1_rect = p1text.get_rect(center=(width/2 - 200, 80))
+    p2_rect = p2text.get_rect(center=(width/2 + 200, 80))
+
+
     # draw the player paddles
-    pygame.draw.rect(screen, "white", playerone)
-    pygame.draw.rect(screen, "white", playertwo)
+    pygame.draw.rect(screen, "white", PlayerOne)
+    pygame.draw.rect(screen, "white", PlayerTwo)
     pygame.draw.circle(screen, "white", ball.center, 10)
 
     screen.blit(p1text, p1_rect)
@@ -120,6 +122,4 @@ while True:
 
     pygame.display.update()
     # clock.tick : Frames per second
-    clock.tick(250)
-
-    
+    clock.tick(300)
